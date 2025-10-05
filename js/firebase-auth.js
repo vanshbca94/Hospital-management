a9e1a85f732c71c8261b5b5370d4fd226ee5f625
vanshbca94 <vp955869i@gmail.com>
2025-10-05 12:48:34 +0530

    Update firebase-auth.js

--- js/firebase-auth.js
+++ js/firebase-auth.js
@@ -1,1 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCjHnNK6pc2KQlY642SqEHXpOMjx0ghfcI",
+  authDomain: "hospital-management-syst-e1875.firebaseapp.com",
+  projectId: "hospital-management-syst-e1875",
+  storageBucket: "hospital-management-syst-e1875.firebasestorage.app",
+  messagingSenderId: "174278534239",
+  appId: "1:174278534239:web:3a9312309aa4e60ea0e7ea",
+  measurementId: "G-2FBFSZE4QB"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
